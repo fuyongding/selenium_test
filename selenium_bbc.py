a292e374a2c01b89aca3ecd246bb6d16d4f2316c
unknown <asherfyd@gmail.com>
2023-11-09 15:16:26 +0800

    first commit

---
+++ selenium_bbc.py
@@ -0,0 +1,46 @@
+import time
+import os
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.options import Options
+
+chrome_options = Options()
+
+chrome_options.add_argument('--disable-notifications')
+chrome_options.add_argument('--disable-infobars')
+chrome_options.add_argument('start-maximized')
+#chrome_options.add_argument('--no-sandbox') 
+
+driver = webdriver.Chrome(options = chrome_options)
+webpage = "https://www.bbc.com/news"
+
+try:
+    driver.get(webpage)
+    driver.implicitly_wait(5)
+
+    # Use the XPath to find the title of headline
+    headline_element = driver.find_element(By.XPATH, "//h3[@class='gs-c-promo-heading__title gel-paragon-bold nw-o-link-split__text']")
+    print(f"TOP HEADLINE: {headline_element.text}")
+
+    print("\n-----------------------------------------------------------------------------------------------------------------\n")
+
+    for i in range(10):
+        list_element = driver.find_element(By.XPATH, f"//li[@data-entityid='most-popular-read-{i+1}']")
+        #print(list_element.get_attribute('outerHTML'))
+
+        span_element = list_element.find_element(By.CSS_SELECTOR, "span.gs-c-promo-heading__title.gel-pica-bold")
+        title = span_element.text
+        #print(span_element.get_attribute('outerHTML'))
+        print(f"{i+1}) {title}")
+    
+    print("\n-----------------------------------------------------------------------------------------------------------------\n")
+except Exception as e:
+    print(e)
+finally:
+    #time.sleep(5) #idk why need this
+    pid = driver.service.process.pid
+    os.system(f'taskkill /PID {pid} /F')
+    driver.quit()  
+
+
+
---
+++ selenium_test.py
@@ -0,0 +1,46 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.common.exceptions import NoSuchElementException
+from selenium.webdriver.chrome.options import Options
+
+chrome_options = Options()
+chrome_options.add_argument('start-maximized')
+
+company_ticker = input("Enter the company ticker symbol: ").upper()
+if company_ticker != '':
+    driver = webdriver.Chrome(options = chrome_options)
+    webpage = f"https://sg.finance.yahoo.com/quote/{company_ticker}"
+    driver.get(webpage)
+
+    driver.implicitly_wait(2)
+
+    try:
+        stock_price_node = driver.find_element(By.XPATH, f'//fin-streamer[@data-symbol="{company_ticker}"][@data-field="regularMarketPrice"]')
+        stock_price = stock_price_node.get_attribute('innerText').strip()
+
+        currency_node = driver.find_element(By.XPATH, "//span[contains(text(), 'Currency in')]") # Find the span element that contains the text "Currency in"
+        #print(currency_node.get_attribute('outerHTML'))
+        currency_text = currency_node.text
+        currency = currency_text.split()[-1]
+
+        print(f'The current price of {company_ticker} is: {stock_price} {currency}')
+
+        driver.save_screenshot(f'images/wholescreen_{company_ticker}.png') # Take a screenshot of the entire page
+
+        canvas_element = driver.find_element(By.XPATH, '//canvas[@role="img"]') 
+        canvas_element.screenshot(f'images/canvas_{company_ticker}.png') # take ss of canvas chart
+
+        chart_link_node = driver.find_element(By.XPATH, "//li[@data-test='CHART']")
+        chart_link_node.click() # Click the element
+
+        driver.implicitly_wait(5)   
+
+        chart_element = driver.find_element(By.XPATH, "//div[contains(@id, 'Lead-7-Chart-Proxy')]")
+        driver.execute_script("arguments[0].scrollIntoView(true);", chart_element)
+        chart_element.screenshot(f'images/chart_{company_ticker}.png')
+    except NoSuchElementException:
+        print(f'Stock price of {company_ticker} could not be found on YAHOO finance')
+    finally:
+        driver.quit()
+else:
+    print("INPUT SOMETHING YOU DUMBASS")
